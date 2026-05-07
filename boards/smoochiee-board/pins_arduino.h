30d7a5aad35336330315d94a6a2d01bb02afaa77
sTs-456 <sitikar707@gmail.com>
2026-05-07 10:57:16 +0700

    Restore Pins_Arduino_h header guard and content

--- boards/smoochiee-board/pins_arduino.h
+++ boards/smoochiee-board/pins_arduino.h
@@ -83,7 +83,7 @@ static const uint8_t SCK = 18;
 #define TFT_MOSI 11
 #define TFT_MISO -1
 #define TFT_CS   -1
-#define TFT_BL   48
+#define TFT_BL   -1
 #define TOUCH_CS -1 // SDCARD_CS to make sure SDCard works
 #define SMOOTH_FONT 1
 #define SPI_FREQUENCY 20000000
