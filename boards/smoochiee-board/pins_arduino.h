11adc70a2d19a578d15481cca93de3e1aee01038
sTs-456 <sitikar707@gmail.com>
2026-05-09 05:52:22 +0700

    Update pins_arduino.h

--- boards/smoochiee-board/pins_arduino.h
+++ boards/smoochiee-board/pins_arduino.h
@@ -83,7 +83,7 @@ static const uint8_t SCK = 18;
 #define TFT_MISO -1
 #define TFT_MOSI 17
 #define TFT_SCLK 18
-#define TFT_CS -1
+#define TFT_CS 7
 #define TFT_SPI_HOST 3
 #define TOUCH_CS -1 // SDCARD_CS to make sure SDCard works
 #define SMOOTH_FONT 1
