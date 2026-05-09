25fa284eca2e48b6195e7f4d5fb73aa89e6340f2
sTs-456 <sitikar707@gmail.com>
2026-05-09 07:27:55 +0700

    Update User_Setup.h

--- lib/TFT_eSPI/User_Setup.h
+++ lib/TFT_eSPI/User_Setup.h
@@ -53,7 +53,7 @@
 // portrait orientation
 // #define TFT_WIDTH 80
 // #define TFT_WIDTH  128
-// #define TFT_WIDTH  240 // ST7789 240 x 240 and 240 x 320
+#define TFT_WIDTH  240 // ST7789 240 x 240 and 240 x 320
 // #define TFT_HEIGHT 160
 // #define TFT_HEIGHT 128
 #define TFT_HEIGHT 240 // ST7789 240 x 240
